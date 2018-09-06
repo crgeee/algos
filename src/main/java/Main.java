5bf4957a42daeaf62e477d618d850bf4a9f1f79e
Christopher Gonzalez <crg167@gmail.com>
2018-09-06 16:06:39 -0700

    boom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2018 SSC Pacific. All rights reserved.
- * The United States Government has unlimited rights to this software.
- * 
- * Distribution Statement D. Distribution authorized to the Department of Defense and
- * U.S. DoD Contractors only in support of US DoD efforts. Other requests shall be
- * referred to Marine Corps Systems Command (MCSC) Marine Air-Ground Task Force 
- * (MAGTF) Command and Control (C2) Systems (MC2S) Program.
- * 
- * Warning: - This document contains data whose export is restricted by the Arms 
- * Export Control Act (Title 22, U.S.C., Sec 2751, et seq.) as amended, or the Export 
- * Administration Act (Title 50, U.S.C., App 2401 et seq.) as amended. Violations of 
- * these export laws are subject to severe criminal and civil penalties. Disseminate 
- * in accordance with provisions of DoD Directive 5230.25. 
- * 
- */
 package main.java;
 
 import tests.java.dailycoding.DailycodingTests;
@@ -28,7 +12,7 @@ public class Main {
 
         LeetcodeTests leetcodeTests = new LeetcodeTests();
         leetcodeTests.execute();
-        
+
         DailycodingTests dailycodingTests = new DailycodingTests();
         dailycodingTests.execute();
     }
