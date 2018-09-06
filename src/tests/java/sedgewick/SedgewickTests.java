ce115383ff31814b7e273b104442a6ca269efa49
Christopher Gonzalez <crg167@gmail.com>
2018-09-06 09:44:11 -0700

    test 2

--- src/tests/java/sedgewick/SedgewickTests.java
+++ src/tests/java/sedgewick/SedgewickTests.java
@@ -58,6 +58,7 @@ public class SedgewickTests {
         //endregion QUICK UNION
 
         
+
         //region QUICK UNION WEIGHTED
         QuickUnionWeightedUF quickUnionWeighted = new QuickUnionWeightedUF(100);
         startTime = System.nanoTime();
