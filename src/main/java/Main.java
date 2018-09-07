75cf485fb25b410823e9a0d07f05d02830771165
Christopher Gonzalez <crg167@gmail.com>
2018-09-07 15:29:16 -0700

    boom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
         LeetcodeTests leetcodeTests = new LeetcodeTests();
         leetcodeTests.execute();
-        
+
         DailycodingTests dailycodingTests = new DailycodingTests();
         dailycodingTests.execute();
     }
